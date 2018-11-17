10b6716f2a08fe35f458501685fa9c75fe9e33bf
gdm-1819-pauldefr <paulien.defraine@student.arteveldehs.be>
2018-11-17 23:20:41 +0100

    add file

---
+++ opdrachten/opdracht4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace opdracht4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+        public class Menu
+        {
+        private string price;
+        private string product;
+        
+        public Menu(string priceValue, string productName)
+        {
+            price = priceValue;
+            product = productName;
+        }
+        
+        // Remaining implementation of Menu class.
+        }
+
+   
+        //Welke klasse is de subklasse? Welke de basisklasse?
+        // De subklasse is product en de basisklasse is Menu
+        //Als je een sortering zou uitvoeren op de elementen van de lijst hoe zou dit dan implementeren?
+    }
+}
