678e0be38f8ec386f4bee6fa8aef5cba9f77c871
gdm-1819-pauldefr <paulien.defraine@student.arteveldehs.be>
2018-11-21 11:58:21 +0100

    add file

---
+++ lesweken/week8/startproject/Drank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace startproject
+{
+    
+        public class Drank : Product
+    {
+        private int hoeveelheid;
+        private bool alcoholAanwezig;
+
+        public int BerekenKCal(){
+    return 10;
+}
+    }
+
+    }
+
---
+++ lesweken/week8/startproject/Snack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace startproject
+{
+    
+        public class Snack : Product
+    {
+        private int hoeveelheid;
+        private bool alcoholAanwezig;
+
+        public int BerekenKCal(){
+    return 20;
+
+    }
+
+    }
+}
