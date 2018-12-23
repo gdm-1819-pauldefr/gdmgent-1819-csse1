ec9b13a1f7893041ff397b2d3420e1690fd104f2
gdm-1819-pauldefr <paulien.defraine@student.arteveldehs.be>
2018-12-23 21:36:50 +0100

    commit

--- opdrachten/opdracht6/Coffee/Program.cs
+++ opdrachten/opdracht6/Coffee/Program.cs
@@ -6,7 +6,109 @@ namespace Coffee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Productcreators */
+            var productCreator1 = new ProductCreator(new EspressoBuilder());
+            productCreator1.CreateProduct();
+            var product1 = productCreator1.GetProduct();
+            product1.ShowInfo();
+            
+            Console.WriteLine("---------------------------------------------");
+            
+            var productCreator2 = new ProductCreator(new MachiattoBuilder());
+            productCreator2.CreateProduct();
+            var product2 = productCreator2.GetProduct();
+            product2.ShowInfo();
+
+            Console.ReadKey();
         }
     }
-}
+    /*productbuildrers + stappen */
+    public interface IProductBuilder{
+        void SetSoort();
+        void SetStap1();
+        void SetStap2();
+        void SetStap3();
+        void SetStap4();
+        Product GetProduct();
+    }
+    /*essproessobuilder */
+    public class EspressoBuilder : IProductBuilder{
+        Product objProduct = new Product();
+        public void SetSoort(){
+            objProduct.soort = "Espresso";
+        }
+        public void SetStap1(){
+            objProduct.stap1 = "Voeg Melk toe";
+        }
+        public void SetStap2() {
+            objProduct.stap2 = "Klop Melk schuim";
+        }
+        public void SetStap3() {
+            objProduct.stap3 = "Voeg espresso toe";
+        }
+        public void SetStap4() {
+            objProduct.stap4 = "Serveer";
+        }
+        public Product GetProduct(){
+            return objProduct;
+        }
+    }
+    /*machiattobuilder */
+        public class MachiattoBuilder : IProductBuilder{
+            Product objProduct = new Product();
+            public void SetSoort(){
+                objProduct.soort = "Machiatto";
+            }
+            public void SetStap1(){
+                objProduct.stap1 = "Voeg Melk toe";
+            }
+            public void SetStap2() {
+                objProduct.stap2 = "Klop Melk schuim";
+            }
+            public void SetStap3() {
+                objProduct.stap3 = "Voeg Machiatto toe";
+            }
+            public void SetStap4() {
+                objProduct.stap4 = "Serveer";
+            }
+            public Product GetProduct(){
+             return objProduct;
+            }
+        }
+        public class Product{
+            public string soort { get; set; }
+            public string stap1 { get; set; }
+            public string stap2 { get; set; }
+            public string stap3 { get; set; }
+            public string stap4 { get; set; }
+            public Product()
+            {
+             
+            }
+            public void ShowInfo(){
+                Console.WriteLine("Soort: {0}", soort);
+                Console.WriteLine("Stap1: {0}", stap1);
+                Console.WriteLine("Stap2: {0}", stap2);
+                Console.WriteLine("Stap3: {0}", stap3);
+                Console.WriteLine("Stap4: {0}", stap4);
+            }
+        }
+            public class ProductCreator{
+            private readonly IProductBuilder objectBuilder;
+            
+            public ProductCreator(IProductBuilder builder){
+                objectBuilder = builder;
+            }
+            
+            public void CreateProduct(){
+                objectBuilder.SetSoort();
+                objectBuilder.SetStap1();
+                objectBuilder.SetStap2();
+                objectBuilder.SetStap3();
+                objectBuilder.SetStap4();
+            }
+            public Product GetProduct(){
+                return objectBuilder.GetProduct();
+            }
+        }
+    }
--- opdrachten/opdracht6/deel2.md
+++ opdrachten/opdracht6/deel2.md
@@ -8,17 +8,72 @@ _Verslag over databanken met als doelvragen:_
 ---
 
 ## Wat is een databank?
-Een databank is een digitale verzameling van gegevens. Hier kunnen we informatie in plaatsen en gaan ophalen.  
-Deze is opgebouwd uit één of meerdere tabellen.
+Een databank is een digitale verzameling van gegevens. Hierin wordt er informatie geplaatst om daarna weer te kunne ophalen. DIt is meestal opgebouwd uit één of meerder tabellen.
+
+
 
 ## Welke databanken zijn er?
-* Hiërarchisch
-* Netwerk
-* Relationele
-* Object-georiënteerde relationele
+- Big data‎ 
+- Blockchains‎ 
+- Creative Commons-licensed databases‎ 
+- Document-oriented databases‎ 
+- Full text databases‎
+- Graph databases‎ 
+- Knowledge bases‎ 
+- Online databases‎
+- Public domain databases‎ 
+- Real-time databases‎
+- Spatial databases‎
+- Temporal databases‎
+- Tz database‎
+- XML databases‎
+
+
 
 ##Wat zijn de verschillen?
-De hiërarchische gaat vooral gaan werken in boomstructuur.  
-Netwerk gaat veel op veel relaties ondersteunen.  
-Relationele Hier zal de er geen veel op veel relatie zijn maar word er gebruik gemaakt van tabellen
-Object-georiënteerderelationele zal de opgeslagen data zo zetten in vorm van een object.
+**Big data**
+> word gebruikt bij grote en belangrijke velden van studie/ grote informatie bedrijven...
+
+**Blockchains‎** 
+> gebruikt bij cryptocurrencies
+
+**Creative Commons-licensed databases**‎ 
+> word gebruikt zodat er meerdere contributers van over de hele wereld kunnen meewerken
+
+**Document-oriented databases‎** 
+> opslaan, ophalen en managing van document georiënteerde info
+    
+**Full text databases**‎ 
+> gebruik om complete boekteksten in op te slaan
+    
+**Graph databases**‎ 
+> gebruikt grafiestructuren voor semantische query's met knooppunten, randen en eigenschappen om gegevens te vertegenwoordigen en op te slaan
+    
+**Knowledge bases**‎ 
+> complexe gestructureerde en niet-gestructureerde data opslaan
+    
+**Online databases**‎ 
+> aanspreekbaar vanop een lokaal netwerk
+    
+**Public domain databases**‎ 
+> free to use databases
+    
+**Real-time databases**‎ 
+> direct aanpast aan de data die word toegevoegd
+    
+**Spatial databases‎** 
+> Een ruimtelijke database
+> geoptimaliseerd voor het opslaan en opvragen van gegevens die zijn gedefinieerd in een geometrische ruimte.
+    
+**Temporal databases**‎ 
+> slaat gegevens op met betrekking tot tijdsinstanties. 
+>Het biedt informatie over verleden, heden en toekomst
+    
+**Tz database**‎ 
+>samenwerkende compilatie van informatie over de tijdzones in de wereld
+>voornamelijk bedoeld voor gebruik met computerprogramma's en besturingssystemen.
+    
+**XML database** 
+> data-persistentiesoftware waarmee gegevens kunnen worden opgegeven 
+>soms opgeslagen in XML-indeling
+    
